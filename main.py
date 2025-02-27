8cad3f13feef4bbc9ad2eeb150c32d362ad0765f
Nicolás Gutiérrez (ElLimitador) <80768621+NicoJGutierrez@users.noreply.github.com>
2025-02-26 16:37:39 -0800

    fix: better prompt

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ client = openai.OpenAI(
 class Answer(BaseModel):
     # Define the answer format
     amount_of_pigs: int
-    estimated_age: list[int]
+    environment: str
     positive_behaviours: list[str]
     negative_behaviours: list[str]
     health_issues: list[str]
@@ -26,11 +26,11 @@ class Answer(BaseModel):
 
 # Define the question
 question = """You are a welfare biologist labelling images of pigs to be used in a dataset for classifing pig welfare. 
-For each image, return succinct notes on these dimensions: number of pigs, positive behaviors, negative behaviors, health issues, estimated age, and posture. Below are examples of some of those dimensions. Only turn concise labels on what you do observe in the image for each dimension.
-Positive behaviors: panting, wallowing, digging, rolling around
+For each image, return succinct notes on these dimensions: number of pigs, environment pigs are in, positive behaviors, negative behaviors, health issues, and posture. Below are examples of each of those dimensions. Only turn concise labels on what you do observe in the image for each dimension.
+Positive behaviors: panting, wallowing, digging, rolling around.
 Negative behavior: pinned-back ears, worried or anxious expression on the face, biting other pig on the flank or tail (as indicated by having their mouth open in the direction of another pig).
 Health issues: open wounds, dolphin-shaped nose, bulging eyes, wrinkles to the mouth, dry “crispy” gums, glazed eyes, drooling from the mouth, bright red skin around the eyes and tongue, bloody tears.
-Posture: dog-sitting position, lying down with legs stretched out, lying down without legs stretched out, feeding, standing, in motion"""
+Posture: sitting position (like a dog sits), lying down with legs stretched out, lying down without legs stretched out, feeding, standing, in motion."""
 
 # Iterate through all image files in the images folder
 image_folder = os.path.join(os.path.dirname(__file__), "images")
